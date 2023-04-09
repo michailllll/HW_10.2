236d410762ae2509a8cfee2aa2f33ceb17b7621a
michailllll <108490552+michailllll@users.noreply.github.com>
2023-04-09 22:05:36 +0300

    Update task_44.py

--- task_44.py
+++ task_44.py
@@ -1,20 +1,20 @@
-# import pandas as pd
+import pandas as pd
 
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
-# random.shuffle(lst)
-# data = pd.DataFrame({'whoAmI' : lst})
-# data.head()
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI' : lst})
+data.head()
 
 
-# lst_2 = []
-# for i in data['whoAmI']:
-#  if i == 'robot':
-#   lst_2.append(0)
-#  else:
-#   lst_2.append(1)
+lst_2 = []
+for i in data['whoAmI']:
+if i == 'robot':
+ lst_2.append(0)
+else:
+ lst_2.append(1)
 
-# data['whoAmIOneHot'] = lst_2
+data['whoAmIOneHot'] = lst_2
 
-# print(data)
+print(data)
